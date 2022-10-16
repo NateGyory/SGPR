bc7d54519294bb958b7a9fb179347c3a03d5e424
NateGyory <natepgyory@gmail.com>
2022-10-15 22:56:46 -0600

    ply file should now be viewable

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,26 @@
-#include<iostream>
-#include "pcl/io/ply_io.h"
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+#include <pcl/io/ply_io.h>
+#include <pcl/conversions.h>
+#include <pcl/visualization/cloud_viewer.h>
+
+using namespace std::chrono_literals;
 
 int main()
 {
     // TODO:
-    // 1) Add Open3D
-    // 2) Render hand in Open3D
-    // 3) Apply VoxelGrid filter to hand
-    std::string handFile = "/home/nate/Datasets/handsPly/b1.ply";
-    pcl::PCLPointCloud2 cloud;
-    pcl::io::loadPLYFile(handFile, cloud);
+    // 1) Get the CloudViewer to work
+    // 2) Apply VoxelGrid filter
+    std::string handFile = "/home/nate/Datasets/teddyPly/test.ply";
+    pcl::PCLPointCloud2 cloud2;
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::io::loadPLYFile(handFile, cloud2);
 
-    std::cout << cloud << std::endl;
+    pcl::fromPCLPointCloud2( cloud2, *cloud);
+
+
+    pcl::visualization::CloudViewer viewer("Cloud Viewer");
+    viewer.showCloud(cloud);
 }
